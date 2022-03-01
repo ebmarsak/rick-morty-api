bb7b3cd9a60a8122a53ece5405cb2909afb95ce7
ebmarsak <emreb.marsak@gmail.com>
2022-03-01 19:16:48 +0300

    Initial Commit

---
+++ rick-morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-morty
+//
+//  Created by Teto on 1.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
